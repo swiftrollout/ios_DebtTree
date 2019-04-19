b0847f3f07909e24e466280d965404a585a4e829
Fung Yung <fungyung1225@gmamil.com>
2019-04-18 21:49:25 -0400

    added Group View and controller

---
+++ DebtTree/GroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewController.swift
+//  DebtTree
+//
+//  Created by Fung on 4/18/19.
+//  Copyright © 2019 fungyung. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
