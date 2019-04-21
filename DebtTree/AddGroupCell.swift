93bf5c9eafdd52b0ace7057d2a85eb963673c283
mike yiu <jjayblmy@gmail.com>
2019-04-21 19:06:03 -0400

    added two screens: New Group and Add Group and their corresponding swift files. on top of Add Group screen, a UIViewCell is created and UIViewCell swift file created.

---
+++ DebtTree/AddGroupCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddGroupCell.swift
+//  DebtTree
+//
+//  Created by Mike Yiu on 4/21/19.
+//  Copyright © 2019 fungyung. All rights reserved.
+//
+
+import UIKit
+
+class AddGroupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
